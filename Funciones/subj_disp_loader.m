5d234830567ca96fe6ad376f5259fc4ac0909606
herediameanuel17 <herediaemanuel17@gmail.com>
2021-04-30 11:22:33 -0300

    Eliminacion de Plataformas de Simulink, Modificar lista de sujetos disponibles, se desactivo la opcion de comidas mixtas en el archivo Escenario/Pruebas. En Simulador UNLP se descomento a todos los pacientes y en loader se dejo "Tutti"

--- Funciones/subj_disp_loader.m
+++ Funciones/subj_disp_loader.m
@@ -5,18 +5,17 @@ sujeto = what('Pacientes'); %lista de archivos MATLAB segun tipo dentro de la ca
 list       = strrep(sujeto.mat,'.mat','');
 
 %Tutti
-% [s,v]      = listdlg('ListString',list,'SelectionMode','multiple','Name','Simulador UNLP','PromptString','Lista de pacientes a simular','InitialValue',1:30); % Interfaz gáfica
-% sujetos     = list(s); % Arreglo de strings con el nombre de cada archivo correspondiente a los pacientes
+ [s,v]      = listdlg('ListString',list,'SelectionMode','multiple','Name','Simulador UNLP','PromptString','Lista de pacientes a simular','InitialValue',1:30); % Interfaz gáfica
+ sujetos     = list(s); % Arreglo de strings con el nombre de cada archivo correspondiente a los pacientes
 
 %Solo adultos
-adult_list = list(strncmp('adult',list,5)); % Extraigo los adultos de la lista
-[s,v]      = listdlg('ListString',adult_list,'SelectionMode','multiple','Name','Simulador UNLP','PromptString','Lista de pacientes a simular','InitialValue',[1]); % Interfaz gáfica
-sujetos    = adult_list(s); % Arreglo de strings con el nombre de cada archivo correspondiente a los pacientes
+%adult_list = list(strncmp('adult',list,5)); % Extraigo los adultos de la lista
+%[s,v]      = listdlg('ListString',adult_list,'SelectionMode','multiple','Name','Simulador UNLP','PromptString','Lista de pacientes a simular','InitialValue',[1]); % Interfaz gáfica
+%sujetos    = adult_list(s); % Arreglo de strings con el nombre de cada archivo correspondiente a los pacientes
 
 aux      = struct2cell(dir('Hardware/*.pmp'));
 bombas   = char(aux(1,:));
 aux      = struct2cell(dir('Hardware/*.scs'));
 sensores = char(aux(1,:));
-
 
 end
